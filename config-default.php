d1ca973d5ccfc7fcbcdecb5f4f41650f8104a94b
Jaime <jmartinez@laespanola.es>
2013-01-16 16:50:18 +0100

    Revert "Revert "Test 2""
    This reverts commit c26e9e5542210eb84237ff099844b16c6a2a1e48.

--- config-default.php
+++ config-default.php
@@ -1,9 +1,15 @@
 <?php
 
+<<<<<<< HEAD
 date_default_timezone_set('Europe/Madrid');
 
 $config['atk']['base_path']='../atk4/';
 $config['dsn']='mysql://myaao:htinez12@aao.laespanola.es/myaao';
 //$config['dsn']='mysql://root:root@192.168.44.13/myaao';
+=======
+$config['atk']['base_path']='../atk4/';
+$config['dsn']='mysql://testsql:Azeitu1@test.laespanola.es/testlaespanola';
+
+>>>>>>> Test 2
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
