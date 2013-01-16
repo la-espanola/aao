946da769f146fecb17b2f9d0db2d99f587f301b3
Jaime <jmartinez@laespanola.es>
2013-01-16 16:49:35 +0100

    Revert "Primer envío"
    This reverts commit 0330748adbd1904605fcc009e3f60d10d18dd924.

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 include '../atk4/loader.php';
-<<<<<<< HEAD
 $api=new Frontend('sample_project');
-=======
-$api=new Frontend('aao');
->>>>>>> Primer envío
 $api->main();
--- page/index.php
+++ page/index.php
@@ -2,12 +2,7 @@
 class page_index extends Page {
     function init(){
         parent::init();
-<<<<<<< HEAD
         
         $this->add('P')->set('Test');
-=======
- 
-        $this->add('P')->set('¡Hola mundo!');
->>>>>>> Primer envío
     }
 }
