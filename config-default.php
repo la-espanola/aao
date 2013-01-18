1bb88cb951280b06be5c5ad9872d587f74b353d3
Jaume <jmartinez@laespanola.es>
2013-01-18 21:57:30 +0100

    .

--- config-default.php
+++ config-default.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Madrid');
 
 $config['atk']['base_path']='../atk4/';
+//$config['dsn']='mysql://myaao:htinez12@aao.laespanola.es/myaao';
 $config['dsn']='mysql://root:eterno@127.0.0.1/myaao';
-//$config['dsn']='mysql://root:root@192.168.44.13/myaao';
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
