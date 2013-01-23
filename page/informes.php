2826bb1a7a0153197c2d3faaa6baeca7983413e9
Jaume <jmartinez@laespanola.es>
2013-01-23 10:38:06 +0100

    Filtro para no mostrar meses anteriores a 9/2012

--- page/informes.php
+++ page/informes.php
@@ -39,40 +39,42 @@ class page_informes extends Page {
         $b=$formDatos->addSubmit('Mostrar');
         $b->js('click',$b->js()->hide());
         
-        $m=$this->add('Model_Informes');   
-        $m->CargarInforme($informe,$ejercicio,$mes,$variedad['id']);
-        
         $tabla=$this->add('HtmlElement')->setElement('table')->setAttr('id','tabla');
-        $var=$this->add('Model_Variedades');
-        
-        $primeraLinea=true;
-        foreach ($var as $variedad) {
-	    	$m=$m->FiltrarDatos($informe, $ejercicio, $mes, $variedad['id']);
-	    	$linea=$tabla->add('HtmlElement')->setElement('tr'); 
-	    	if ($primeraLinea) {
-	    		$linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set($informe.' '.$ejercicio.' '.$mes);
-	    		$estado_anterior='';
+        	
+        $m=$this->add('Model_Informes');
+        if ($ejercicio>2012 
+        	|| ($ejercicio==2012 && $mes>8)) {
+	        $m->CargarInforme($informe,$ejercicio,$mes,$variedad['id']);
+	        $var=$this->add('Model_Variedades');
+	        
+	        $primeraLinea=true;
+	        foreach ($var as $variedad) {
+		    	$m=$m->FiltrarDatos($informe, $ejercicio, $mes, $variedad['id']);
+		    	$linea=$tabla->add('HtmlElement')->setElement('tr'); 
+		    	if ($primeraLinea) {
+		    		$linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set($informe.' '.$ejercicio.' '.$mes);
+		    		$estado_anterior='';
+			    	foreach ($m as $datos) {
+			    		if ($estado_anterior=='') $linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set('VERDE');
+			    		else if ($estado_anterior=='V' && $datos['destinos_id']!='V') $linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set('NEGRA');
+			    		$linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set($datos['apartado']);
+			    		$estado_anterior=$datos['destinos_id'];
+			    	}
+			    	$linea=$tabla->add('HtmlElement')->setElement('tr'); 
+			    	$primeraLinea=false;	
+		    	}
+		    	$linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set($variedad['name']);
+		    	$estado_anterior='';
 		    	foreach ($m as $datos) {
 		    		if ($estado_anterior=='') $linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set('VERDE');
 		    		else if ($estado_anterior=='V' && $datos['destinos_id']!='V') $linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set('NEGRA');
-		    		$linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set($datos['apartado']);
-		    		$estado_anterior=$datos['destinos_id'];
-		    	}
-		    	$linea=$tabla->add('HtmlElement')->setElement('tr'); 
-		    	$primeraLinea=false;	
-	    	}
-	    	$linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set($variedad['name']);
-	    	$estado_anterior='';
-	    	foreach ($m as $datos) {
-	    		if ($estado_anterior=='') $linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set('VERDE');
-	    		else if ($estado_anterior=='V' && $datos['destinos_id']!='V') $linea->add('HtmlElement')->setElement('td')->addClass('titulo')->set('NEGRA');
-		    	$linea->add('HtmlElement')->setElement('td')->addClass('numero')->set(number_format($datos['kilos']));
-		    	$estado_anterior=$datos['destinos_id'];
-	    	}	
-        }
-        
-        $tabla->js(true)->_load('transpose_table')->univ()->transposeTable('aao_informes_htmlelement');
-                         
+			    	$linea->add('HtmlElement')->setElement('td')->addClass('numero')->set(number_format($datos['kilos']));
+			    	$estado_anterior=$datos['destinos_id'];
+		    	}	
+	        }
+	        
+	        $tabla->js(true)->_load('transpose_table')->univ()->transposeTable('aao_informes_htmlelement');
+		}	                         
         if ($formDatos->isSubmitted()) {
             $tabla->js(null,$tabla->js(null, $b->js()->show()))->reload(array('informe'=>$formDatos->get('informe'),
             				'mes'=>$formDatos->get('mes'),
