3bf3a6229dcc6fe3db5f6a9a1fe4c493d5f5b878
Jaume <jmartinez@laespanola.es>
2013-02-19 12:23:35 +0100

    Cambio de plantilla

--- config-default.php
+++ config-default.php
@@ -4,6 +4,6 @@ date_default_timezone_set('Europe/Madrid');
 
 $config['atk']['base_path']='../atk4/';
 //$config['dsn']='mysql://myaao:htinez12@aao.laespanola.es/myaao';
-$config['dsn']='mysql://root@127.0.0.1/myaao';
+$config['dsn']='mysql://root:eterno@127.0.0.1/myaao';
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
